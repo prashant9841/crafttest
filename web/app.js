eae95b92329916dd137e02c218ae6ec538986949
Prashant Sharma <itachi9841@gmail.com>
2021-07-13 11:36:56 +1200

    dev: initial build

---
+++ web/app.js
@@ -0,0 +1,26 @@
+$(window).on("load", function () {
+	var wHeight = $(window).height();
+	var wWidth = $(window).width();
+
+	$(".loading").fadeOut("3000");
+
+	$(".slickSlider").slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		fade: true,
+
+		autoplay: true,
+		dots: true,
+		autoplaySpeed: 2000,
+	});
+});
+$(window).on("scroll", function () {
+	var wScroll = $(window).scrollTop();
+
+	if (wScroll > 568) {
+		$(".main-header").addClass("show");
+	} else {
+		$(".main-header").removeClass("show");
+	}
+});
